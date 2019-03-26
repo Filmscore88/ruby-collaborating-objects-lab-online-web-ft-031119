747bcfa566cc33b2199d5fd5627ecfab65e08d48
Alberto Morales <cmtmorales24@gmail.com>
2019-03-26 00:27:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ def self.new_by_filename(file_name)
   name=file_name.split(" - ")[1]
   artist=file_name.split(" - ")[0]
   song=Song.new(name)
-  song.artist=  Artist.find(artist)? Artist.find(artist).add_song(song) :  self.create(artist)
+  song.artist=  Artist.find(artist)? Artist.find(artist).add_song(song) :  Artist.create(artist)
   song
 end 
 
