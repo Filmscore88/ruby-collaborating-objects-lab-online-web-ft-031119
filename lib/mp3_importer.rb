f47ce92fb9898348a780917f0b32966e65d00653
Alberto Morales <cmtmorales24@gmail.com>
2019-03-26 00:32:20 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,28 +1,22 @@
-require 'pry'
-class MP3Importer 
-attr_accessor :path 
+class MP3Importer
+  attr_accessor :path
 
-def initialize(path)
-@path= path 
+  def initialize(path)
+    @path = path
+  end
 
-end 
+  def files
+    files = []
+    Dir.new(self.path).each do |file|
+      files << file if file.length > 4
+    end
+    files
+  end
 
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
+  end
+end
 
-
-def files
-new_files=[]
-  Dir.new(self.path).each { |file|
-  new_files << file if file.length > 4 
-  }
-  new_files
-end 
-
-def import 
- 
-  self.files.each {|file|
-  Song.new_by_filename (file)
-  }
-end 
-
-
-end 
