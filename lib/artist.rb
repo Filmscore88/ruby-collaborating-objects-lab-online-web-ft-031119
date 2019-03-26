4b93f37d9f2f580a44228a90b706ce11872b0696
Alberto Morales <cmtmorales24@gmail.com>
2019-03-26 00:29:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,50 +1,46 @@
-require 'pry'
-class Artist 
+class Artist
+  attr_accessor :name, :songs
+  @@all = []  #stores all instances of Artist 
 
-attr_accessor :name 
-@@all=[]
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
+  def add_song(song)
+    self.songs << song    #Artist.songs reader returns all songs 
+                          #stored in @songs
+  end
 
-def initialize(name)
-@name = name
-@songs=[]
-@@all << self
-end 
+  def save
+    @@all << self     #saves all created instances of Artist class in 
+                      #@@all class variable
+  end
 
-def self.all 
-  @@all
-end 
+  def self.all      #class method all returns all instances of Artist class 
+    @@all
+  end
 
-def songs
-  @songs 
-end 
+  def self.find_or_create_by_name(name)   #class method uses find/create class 
+                                          #methods to detect or create instances
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
 
-def save 
-  @@all << self 
-end 
+  def self.find(name)     #class method detects instances from @@all class variable
+    self.all.detect { |artist| artist.name == name }
+  end
 
-def add_song(song)
-@songs<< song
-end 
+  def self.create(name)   #class method creates & stores instances vs initializing
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
 
-def self.find_or_create_by_name(artist)
- self.find(artist)? self.find(artist) : self.create(artist)
-end 
-
-def self.create(name)
-  newArtist= self.new(name)
-  newArtist
-end 
-
-def self.find(name)
- @@all.detect {|artists| artists.name == name}
-
-end  
-
-
-def print_songs
-  self.songs.each{|songs| puts songs.name}
-end 
-  
-
-end 
+  def print_songs #instance method iterates through @songs of an instance of Artist
+    self.songs.each {|song| song.name}
+  end
+end
